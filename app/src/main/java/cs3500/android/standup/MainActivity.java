2e221860bee17bc21945092471d7acf5212d4f1d
Elisa Danthinne <danthinne.e@husky.neu.edu>
2017-02-11 23:52:52 -0500

    menu and json parsing simple

--- app/src/main/java/cs3500/android/standup/MainActivity.java
+++ app/src/main/java/cs3500/android/standup/MainActivity.java
@@ -1,5 +1,7 @@
 package cs3500.android.standup;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,19 +18,26 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import static android.R.id.list;
+import static android.R.id.text1;
+import static android.media.CamcorderProfile.get;
 
 public class MainActivity extends AppCompatActivity {
     private ListView lv;
     private JSONObject jsonObject;
     private JSONArray articles;
-    private List<String> toRender;
+    private List<Article> toRender;
 
     public MainActivity() {
-        this.toRender = new ArrayList<>();
+        this.toRender = new ArrayList<Article>();
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
         download.execute();
         while (download.json == "") {
         }
-
         //the data is returned in the format of a jsonObject, the very last field of
         //the jsonObject is articles
         //articles
@@ -58,21 +66,50 @@ public class MainActivity extends AppCompatActivity {
             Log.e("ERROR", e.getMessage(), e);
             e.printStackTrace();
         }
+        ArrayAdapter<Article> adapter = new ArrayAdapter<Article>(this, android.R.layout.simple_list_item_2, android.R.id.text1, this.toRender) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                Log.d("yo","in get view");
+                View view = super.getView(position, convertView, parent);
+                TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+                TextView text2 = (TextView) view.findViewById(android.R.id.text2);
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.text, this.toRender);
+                text1.setText(toRender.get(position).title);
+                text2.setText(toRender.get(position).author);
+                return view;
+            }
+        };
+        //ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2, this.toRender);
         lv = (ListView) findViewById(R.id.listen_up);
         lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // your code
+                JSONObject obj = null;
+                String my_URL = "";
+                try {
+                    my_URL = ((JSONObject) articles.get(position)).get("url").toString();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(my_URL));
+                startActivity(browserIntent);
+            }
+        });
+
     }
 
-    private void getListViewString(JSONArray jsonArray, List<String> ret) throws JSONException {
+    private void getListViewString(JSONArray jsonArray, List<Article> ret) throws JSONException {
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONObject obj = (JSONObject) jsonArray.get(i);
-            ret.add(obj.get("author").toString());
-            ret.add(obj.get("title").toString());
-            ret.add(obj.get("description").toString());
-            ret.add(obj.get("url").toString());
-            ret.add(obj.get("urlToImage").toString());
-            ret.add(obj.get("publishedAt").toString());
+            //ret.add(obj.get("author").toString());
+            ret.add(new Article(obj.get("title").toString(), obj.get("author").toString()));
+            //ret.add(obj.get("description").toString());
+            //ret.add(obj.get("url").toString());
+            //ret.add(obj.get("urlToImage").toString());
+            //ret.add(obj.get("publishedAt").toString());*/
         }
     }
 
@@ -83,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
         }
         protected String doInBackground(Void... params) {
             try {
-                URL url = new URL("https://newsapi.org/v1/articles?source=techcrunch&apiKey=de48e3ded2c640b18c2e8136c0e314d1");
+                URL url = new URL("https://newsapi.org/v1/articles?source=bbc-sport&apiKey=de48e3ded2c640b18c2e8136c0e314d1");
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 try {
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
@@ -104,6 +141,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
 }
 
 
