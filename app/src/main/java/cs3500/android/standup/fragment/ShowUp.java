ae715c2a60a61284a4cd89b284221afd0362a6fd
feliciazhang <zhang.fel@husky.neu.edu>
2017-02-12 14:26:38 -0500

    show up crashes

--- app/src/main/java/cs3500/android/standup/fragment/ShowUp.java
+++ app/src/main/java/cs3500/android/standup/fragment/ShowUp.java
@@ -32,10 +32,7 @@ import com.facebook.HttpMethod;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-//import com.google.android.gms.appindexing.Action;
-//import com.google.android.gms.appindexing.AppIndex;
-//import com.google.android.gms.appindexing.Thing;
-//import com.google.android.gms.common.api.GoogleApiClient;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -44,6 +41,8 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 /**
  * Created by baharsheikhi on 2/11/17.
@@ -58,7 +57,7 @@ import java.util.List;
 
 public class ShowUp extends Fragment{
 
-     /*   private JSONObject jsonObject1;
+        private JSONObject jsonObject1;
         private JSONObject jsonObject2;
         private TextView info;
         private LoginButton loginButton;
@@ -69,23 +68,18 @@ public class ShowUp extends Fragment{
         private ShowUp me;
         private View rootView;
         ArrayAdapter<String> adapter;
-        /**
-         * ATTENTION: This was auto-generated to implement the App Indexing API.
-         * See https://g.co/AppIndexing/AndroidStudio for more information.
-         */
-    /*    private GoogleApiClient client;
+
 
         public ShowUp() {
             this.ret = new ArrayList<String>();
             this.me = this;
         }
+
         @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.show_up);
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
             FacebookSdk.sdkInitialize(getApplicationContext());
             callbackManager = CallbackManager.Factory.create();
-            setContentView(R.layout.show_up);
             info = (TextView) rootView.findViewById(R.id.info);
             lv = (ListView) rootView.findViewById(R.id.event_list);
             loginButton = (LoginButton) rootView.findViewById(R.id.login_button);
@@ -93,10 +87,11 @@ public class ShowUp extends Fragment{
                     this,
                     Arrays.asList("email"));
             loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-                @Override
-                public void onSuccess(LoginResult loginResult) {
-                    loginButton.setVisibility(View.GONE);
-                    accessToken = loginResult.getAccessToken();
+                        @Override
+                        public void onSuccess(LoginResult loginResult) {
+                            loginButton.setVisibility(View.GONE);
+                            accessToken = loginResult.getAccessToken();
+
                /* info.setText(
                         "User ID: "
                                 + loginResult.getAccessToken().getUserId()
@@ -106,7 +101,7 @@ public class ShowUp extends Fragment{
                 );*/
 
                         /* make the API call */
-              /*      new GraphRequest(accessToken,
+                    new GraphRequest(accessToken,
                             "385640398156882?fields=posts{message},events",
                             null,
                             HttpMethod.GET,
@@ -115,7 +110,7 @@ public class ShowUp extends Fragment{
                         /* handle the result */
 
                                     //final List<String> ret = new ArrayList<String>();
-                    /*                jsonObject1 = response.getJSONObject();
+                                   jsonObject1 = response.getJSONObject();
                                     try {
                                         JSONObject posts = (JSONObject) jsonObject1.get("posts");
                                         JSONArray dataPosts = (JSONArray) posts.get("data");
@@ -148,7 +143,7 @@ public class ShowUp extends Fragment{
                             new GraphRequest.Callback() {
                                 public void onCompleted(GraphResponse response) {
                         /* handle the result */
-           /*                         jsonObject2 = response.getJSONObject();
+                                    jsonObject2 = response.getJSONObject();
                                     //Log.d("Second JSON", jsonObject2.toString());
                                     adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1, ret) {
                                         @Override
@@ -198,10 +193,6 @@ public class ShowUp extends Fragment{
                 }
             });
 
-
-            // ATTENTION: This was auto-generated to implement the App Indexing API.
-            // See https://g.co/AppIndexing/AndroidStudio for more information.
-            client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
         }
 
     public JSONObject firstJSON() {
@@ -213,53 +204,25 @@ public class ShowUp extends Fragment{
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
-    }
-
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-  /*  public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("ShowUp Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
     }
 
     @Override
     public void onStart() {
         super.onStart();
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
     }
 
     @Override
     public void onStop() {
         super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
     }
-}
 
-    }*/
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View rootView = inflater.inflate(R.layout.show_up, container, false);
+        rootView = inflater.inflate(R.layout.show_up, container, false);
         return rootView;
     }
 }
